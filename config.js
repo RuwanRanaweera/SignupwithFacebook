92b5cb614d8e4ba1b7cef4f6b550b3b91783ac73
Ruwan Ranaweera <rwnranaweera@gmail.com>
2022-05-31 08:09:27 +0530

    fb details using for signup

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID':  '1067105124240318', // your App ID
+		'clientSecret':  '2bde92d9b8d5c3b97098fd7c76c15b2c', // your App Secret
+		'callbackURL':  'http://localhost:3000/auth/facebook/callback'
+	}
+}
